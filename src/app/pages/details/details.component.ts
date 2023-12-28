5a58f08785caca0e6834f49bcb1eea3b763e6436
kaunda gama <kaunda.gama@ideiasdinamicas.com>
2023-12-28 09:40:41 +0100

    Featuring of details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from '../../service/poke-api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  private urlPokemon = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
 
   ngOnInit(): void {
+    this.pokemon;
   }
+  get pokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
 
+    const pokemon = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+    return forkJoin([name, pokemon]).subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
